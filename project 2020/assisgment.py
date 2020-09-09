0e73a65710b990f27715c37ddecf8d7c7582567e
sagang tanwouo achille <a.tanwouo@alustudent.com>
2020-09-09 19:01:14 +0200

    adding my first project

---
+++ project 2020/assisgment.py
@@ -0,0 +1,12 @@
+print("SSSSSSSSSSSSSSSSS,   TTTTTTTTTTTTTTTTTT , AAAAAAAAAAAAAAAAA")
+print("S                           TT            AA             AA")
+print("S                           TT            AA             AA")
+print("SSSSSSSSSSSSSSSSS,          TT            AAAAAAAAAAAAAAAAA")
+print("                S           TT            AA             AA")
+print("                S           TT            AA             AA ")
+print("SSSSSSSSSSSSSSSSS           TT            AA             AA ")
+
+
+
+
+
---
+++ project 2020/main.py
@@ -0,0 +1,9 @@
+print("SSSSSSSSSSSSSSSSS,   TTTTTTTTTTTTTTTTTT , AAAAAAAAAAAAAAAAA")
+print("S                           TT            AA             AA")
+print("S                           TT            AA             AA")
+print("SSSSSSSSSSSSSSSSS,          TT            AAAAAAAAAAAAAAAAA")
+print("                S           TT            AA             AA")
+print("                S           TT            AA             AA")
+print("SSSSSSSSSSSSSSSSS           TT            AA             AA")
+
+
